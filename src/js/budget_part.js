7859881b33624909a69ec83dd644bce3390e3976
skconan <supakit.kr@gmail.com>
2019-05-21 14:28:03 +0700

    add pie chart

---
+++ src/js/budget_part.js
@@ -0,0 +1,23 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['จังหวัด', 'งบประมาณ'],
+        ['ระนอง', 4704611.3],
+        ['พังงา', 4897205.225],
+        ['กระบี่', 6240254.7],
+        ['ตรัง', 7203860.7],
+        ['ภูเก็ค', 2407366.4],
+    ]);
+
+    var options = {
+        title: 'งบประมาณแบ่งตามกระทรวง',
+        pieHole: 0.5
+    };
+
+    data.sort([{ column: 1 }]);
+
+    var chart = new google.visualization.PieChart(document.getElementById('ministry_pie'));
+    chart.draw(data, options);
+}
